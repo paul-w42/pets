c9270a57af2b381d4cf2af4e9f7455be9378846e
Paul Woods <paul.n.woods@gmail.com>
2023-01-12 17:25:57 -0500

    Added controller

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+// This is my CONTROLLER for the hello project
+
+// Turn on error reporting (probably want off when live)
+// this will turn it on for each page in our project
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once("vendor/autoload.php");
+
+// Create an instance of the Base class
+$f3 = Base::instance();     // i.e. Base f3 = new Base() in java
+
+
+// Define a default route
+$f3->route('GET /', function() {
+    echo '<h1>Pet Home</h>';
+    //$view = new Template();
+    //echo $view->render('views/home.html');
+});
+
+// Run Fat-Free
+$f3->run();                 // -> is the object operator, equiv to . in java
+
+
+?>
